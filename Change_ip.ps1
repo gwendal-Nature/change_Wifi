7e70c0f5bea02d8a53b30dd4eebc3755caed8fc1
Gwendal_NATURE <gwendal.nature@supdevinci-edu.fr>
2023-03-30 15:30:12 +0200

    test

--- Change_ip.ps1
+++ Change_ip.ps1
@@ -1,60 +1,60 @@
-# Définition des variables pour le réseau
 $compteur = 1
-$IP_arriver = "10.101.7"
-$gateway = "10.101.7.254"
+$IP_arriver = 10.101.7
+$gateway = 10.101.7.254
 $Debit_max = 10000000
 
-# Désactive DHCP sur l'interface réseau
-Set-NetIPInterface -InterfaceIndex 12 -Dhcp Disabled
 
-# Boucle infinie pour surveiller l'état de la carte réseau WiFi
+# Boucle infinie
 while($true)
 {
-    # Récupère la première interface réseau WiFi connectée
+    # Récupère la première interface réseau "Ethernet" et "connecté"
     $interface_wifi = ([System.Net.NetworkInformation.Networkinterface]::GetAllNetworkInterfaces()) | Where-Object {$_.NetworkInterfaceType -eq [System.Net.NetworkInformation.NetworkInterfaceType]::Wireless80211 -and $_.OperationalStatus -eq [System.Net.NetworkInformation.OperationalStatus]::Up} | Select-Object -First 1
 
-    # Si une carte réseau WiFi est trouvée
+    # Si carte réseau trouvé
     if($interface_wifi -ne $null)
     {
-        # Récupère l'ID de l'interface, les statistiques réseau et la date actuelle
+        # Récupère les informations
         $interface_recupe = $interface_wifi.id
         $ipstat_New = $interface_wifi.GetIPStatistics()
         $date = Get-Date
 
-        # Vérifie si le débit peut être calculé entre la dernière mesure et la nouvelle :
-        # même carte réseau, données disponibles
+        # Vérifie si on peux calculer le débit entre la précédente mesure et la nouvelle : Même carte réseau, données présentent
         if ($ipstat_Old -ne $null -and $ipstat_New -ne $null -and $interface_wifiIdOld -eq $interface_recupe)
         {
-            # Calcul du ratio pour les octets/seconde
-            $ratio = 1000 / ($date - $dateOld).TotalMilliseconds
+            # Définit le ratio pour calculer en Octets/seconde
+            $Debit = 1000 / ($date - $dateOld).TotalMilliseconds
 
-            # Récupère et calcule les données à afficher
+            # Récupère et calcul les données à afficher
             $Nom_interface = $interface_wifi.Name
             $DateShow = $date.ToString()
             [int64]$sent = ($ipstat_New.BytesSent - $ipstat_Old.BytesSent) * $ratio
             [int64]$received = ($ipstat_New.BytesReceived - $ipstat_Old.BytesReceived) * $ratio
 
-            # Affiche les résultats : nom de l'interface, date, débit d'envoi et de réception
+            # affiche le résultat de la date l'interface sélectionné et la demande de wifi
             Write-Host "$Nom_interface`n  Date : `t`t$DateShow`n  Envoyés : `t$sent Octets/seconde`n  Reçus : `t`t$received Octets/seconde`n`n"
         }
-        
-        # Si le débit demandé pour le réseau WiFi est supérieur à 10 Mb/s, change l'adresse IP de l'ordinateur vers un réseau WiFi plus performant
-        if ($sent -gt $Debit_max -and $received -gt $Debit_max)
+        # modification de l'ip si le débit demander pour le réseaux wifi est supérieur à 10 Mg alors l'ip de l'ordianteur iras 
+        # vers un wifi plus performant
+        if ( $debit -gt $Debit_max )
         {    
-            New-NetIPAddress –InterfaceIndex 12 –IPAddress ($IP_arriver + $compteur) –PrefixLength 24 -DefaultGateway $gateway
-            $compteur += 1
+            New-NetIPAddress –InterfaceIndex 12 –IPAddress $IP_arriver + $compteur –PrefixLength 24 -deflautgateway $gateway
+            $compteur =+ 1
         }
+        
+
     }
-    # Si aucune carte réseau WiFi n'est trouvée, force les informations à null
+    # Si pas de carte réseau trouvé on force les informations à null
     else
     {
         $interface_recupe = $null
         $ipstat_New = $null
         $date = $null
     }
-    
-    # Stocke les nouvelles données en tant que données précédentes
+    # Les nouvelles données deviennes les anciennes
     $interface_wifiIdOld = $interface_recupe
     $ipstat_Old = $ipstat_New
-    $dateOld = $
+    $dateOld = $date
+
+    # Attend 5 secondes
+    Start-Sleep -Seconds 5
 }
