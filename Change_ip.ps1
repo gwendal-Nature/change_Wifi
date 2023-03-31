c1a86b7d9c5aee444b083882f6a23a5b96286291
Gwendal_NATURE <gwendal.nature@supdevinci-edu.fr>
2023-03-31 15:14:44 +0200

    essaye avec le ssid

--- Change_ip.ps1
+++ Change_ip.ps1
@@ -1,30 +1,33 @@
-$compteur = 1
-$IP_arriver = "10.101.7."
-$gateway = "10.101.7.254"
-$Affichage_debit_max = 100000
-$message = "Votre wifi à changé pour avoir un meuilleur débit si vous avez lancé un téléchargement sur internet il faut le relancé Bonne journée. L'admini sys()"
+$ssid = "ISI_Etudiants"
+$securityKey = "KhDGio!45d "
+$Debit_max = 9 # chiffre maximum que le réseaux ne doit pas dépasser autrement l'appareil sera changé de wifi 
+$message_interface_wifi_up = "Votre wifi à changé pour avoir un meilleur débit si vous avez lancé un téléchargement sur internet il faut le relancé Bonne journée.          L'admini sys"
+$message_interface_wifi_down = "votre wifi est deésactivé pour que le script fonction il faut activé le wifi. Admini sys "
+
 
 # Boucle infinie
-while($true) 
-{
-    # Récupère la première interface réseau "Ethernet" et "connecté"
-    $interface_wifi = ([System.Net.NetworkInformation.Networkinterface]::GetAllNetworkInterfaces()) | Where-Object {$_.NetworkInterfaceType -eq [System.Net.NetworkInformation.NetworkInterfaceType]::Wireless80211 -and $_.OperationalStatus -eq [System.Net.NetworkInformation.OperationalStatus]::Up} | Select-Object -First 1
+while ($true) {
+    # Récupère la première interface réseau "wirless" et "connecté"
+    $interface_wifi = ([System.Net.NetworkInformation.Networkinterface]::GetAllNetworkInterfaces()) | Where-Object { $_.NetworkInterfaceType -eq [System.Net.NetworkInformation.NetworkInterfaceType]::Wireless80211 -and $_.OperationalStatus -eq [System.Net.NetworkInformation.OperationalStatus]::Up } | Select-Object -First 1
 
     # Si carte réseau trouvé
-    if($null -ne $interface_wifi)
-    {
+    if ($null -ne $interface_wifi) {
+        
+
         # Récupère les informations
         $interface_recupe = $interface_wifi.id
         $ipstat_New = $interface_wifi.GetIPStatistics()
         $date = Get-Date
 
         # Vérifie si on peux calculer le débit entre la précédente mesure et la nouvelle : Même carte réseau, données présentent
-        if ($null -ne $ipstat_Old -and $null -ne $ipstat_New -and $interface_wifiIdOld -eq $interface_recupe)
-        {
-
-            # Définit le ratio pour calculer en Mega/seconde
-            $Affichage_debit = 0.01 / ($date - $dateOld).TotalMilliseconds
-
+        if ($null -ne $ipstat_Old -and $null -ne $ipstat_New -and $interface_wifiIdOld -eq $interface_recupe) {
+            $test
+            
+            # Définit le ratio pour calculer en octet/seconde
+            
+            $Affichage_debit = 10000 / ($date - $dateOld).TotalMilliseconds
+            
+            $Affichage_debit
             # Récupère et calcul les données à afficher
             $Nom_interface = $interface_wifi.Name
             $DateShow = $date.ToString()
@@ -32,27 +35,33 @@ while($true)
             [int64]$received = ($ipstat_New.BytesReceived - $ipstat_Old.BytesReceived) * $Affichage_debit
 
             # affiche le résultat de la date l'interface sélectionné et la demande de wifi
-            Write-Host "$Nom_interface`n  Date : `t`t$DateShow`n  Envoyés : `t$sent Mega/seconde`n  Reçus : `t`t$received Mega/seconde`n`n"
+            Write-Host "$Nom_interface`n  Date : `t`t$DateShow`n  Envoyés : `t$sent octet/seconde`n  Reçus : `t`t$received octet/seconde`n`n"
         }
         # modification de l'ip si le débit demander pour le réseaux wifi est supérieur à 10 Mg alors l'ip de l'ordianteur iras 
         # vers un wifi plus performant
-        if ( $Affichage_debit -gt $Affichage_debit_max )
-        {    
-            $newIP = $IP_arriver + $compteur
-            New-NetIPAddress –InterfaceIndex $interface_wifi.ifIndex –IPAddress $newIP –PrefixLength 24 -DefaultGateway $gateway
+        
+        if ( $Affichage_debit -gt $Debit_max ) {  
+            
+            # Créer un profil réseau pour le SSID spécifié
+            Add-WifiProfile  -SSID $ssid -KeyMaterial $securityKey
+
+            # Se connecter au réseau Wi-Fi
+            Connect-Wifi -SSID $ssid
             $compteur++
             $shell = new-object -com wscript.shell
-            $shell.popup("$message")
+            $shell.popup("$message_interface_wifi_up")
         }
         
 
     }
     # Si pas de carte réseau trouvé on force les informations à null
-    else
-    {
+    else {
         $interface_recupe = $null
         $ipstat_New = $null
         $date = $null
+        $shell = new-object -com wscript.shell
+        $shell.popup("$message_interface_wifi_down")
+
     }
     # Les nouvelles données deviennent les anciennes
     $interface_wifiIdOld = $interface_recupe
@@ -60,5 +69,6 @@ while($true)
     $dateOld = $date
 
     # Attend 5 secondes
-    Start-Sleep -Seconds 5
+    Start-Sleep -Seconds 1
+    $test
 }
